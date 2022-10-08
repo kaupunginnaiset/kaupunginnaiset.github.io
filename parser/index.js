d7629b443d819ba712711bb8876b5dbfc0bc3718
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2022-10-08 10:17:58 +0300

    Add initial hugo site.

---
+++ parser/index.js
@@ -0,0 +1,58 @@
+const fs = require('fs')
+const basePath = './data/fb';
+const target = './site/content/fi/post'
+
+if (fs.existsSync(target)) {
+    fs.rmSync(target, {
+        recursive: true,
+        force: true
+    })
+}
+fs.mkdirSync(target)
+
+const filePaths = fs.readdirSync(basePath)
+    .reduce((result, dir) => [
+        ...result,
+        ...fs.readdirSync(`${basePath}/${dir}`)
+            .reduce((mResult, mDir) => [
+                ...mResult,
+                ...fs.readdirSync(`${basePath}/${dir}/${mDir}`)
+                    .filter(filename => filename.endsWith(".json"))
+                    .map(filename => `${basePath}/${dir}/${mDir}/${filename}`)
+            ], [])
+    ], [])
+
+const posts = filePaths.reduce((result, path) => {
+    const json = JSON.parse(fs.readFileSync(path))
+        .filter(({ message }) => message)
+        .map(item => ({ filePath: path, ...item }))
+    return [...result, ...json]
+}, [])
+
+posts.forEach((item => {
+    const timestamp = new Date(item.created_time)
+    const year = timestamp.getFullYear()
+    const content = `
+---
+date: ${item.created_time}
+featured_image: "/feed/${year}/${item.id}.jpg"
+disable_share: true
+---
+
+${item.message || ""}
+
+{{< figure src = "/feed/${year}/${item.id}.jpg" >}}
+
+`
+    const folder = `${target}/${year}`
+    if (!fs.existsSync(folder)) {
+        fs.mkdirSync(folder)
+    }
+
+    fs.writeFileSync(`${folder}/${item.created_time}.md`, content)
+    const lastIndex = item.filePath.lastIndexOf('/')
+    const picPath = `${item.filePath.substring(0, lastIndex)}/${item.id}.jpg`
+    if (fs.existsSync(picPath)) {
+        fs.copyFileSync(picPath, `${folder}/${item.id}.jpg`)
+    }
+}))
---
+++ site/content/fi/_index.md
@@ -0,0 +1,10 @@
+---
+title: "Kaupungin Naiset"
+
+description: "Suomen ainoa lesbokuoro"
+theme_version: '2.8.2'
+cascade:
+  featured_image: '/images/kuoro.jpg'
+---
+Kaupungin Naiset – lauluja elämästä, rakkaudesta ja näiden päättymisestä. Suomen ainoa lesbokuoro esittää suomalaisiin runoihin kuorolle sävellettyä musiikkia – ripauksella huumoria ja patetiaa. Kaupungin Naiset ovat rikkoneet seksuaali- ja sukupuolivähemmistöihin liittyviä stereotypioita sekä tuoneet lesbonäkökulmaa kuorolauluun jo vuodesta 2014.
+
